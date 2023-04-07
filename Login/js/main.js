c9075ac41f37f9c45c10e3abffa57ab748451030
“DinhB2012494” <lelocdinh30072002@gmail.comgit config --global user.email lelocdinh30072002@gmail.com>
2023-04-07 22:47:26 +0700

    upload source

---
+++ Login/js/main.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll(".input");
+
+function thongbao(){
+	alert("Tính năng đang update")
+}
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
